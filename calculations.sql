9d53e8b7165dd4f21c0ed040796512c76c4be191
Anna Ostropolets <anna.ostropolets@odysseusinc.com>
2018-12-26 03:09:24 -0500

    Update calculations.sql

--- calculations.sql
+++ calculations.sql
@@ -20,3 +20,12 @@ select count(*), gender, BMI from ohdsi_cumc_deid_pending.results.events_gender 
 group by gender, BMI;
 select count(*), gender, DM2 from ohdsi_cumc_deid_pending.results.events_gender --
 group by gender, DM2;
+select count(*), gender, AF from ohdsi_cumc_deid_pending.results.events_gender --
+group by gender, AF;
+select count(*), gender, LL from ohdsi_cumc_deid_pending.results.events_gender --
+group by gender, LL;
+
+select count(*),gender, ethnicity_concept_id   from ohdsi_cumc_deid_pending.results.events_gender e
+                                join ohdsi_cumc_deid_pending.dbo.person p
+on e.person_id = p.person_id
+group by gender,ethnicity_concept_id
